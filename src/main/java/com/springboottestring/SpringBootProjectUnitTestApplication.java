a55a9f148452e268c6f48a0d9d53ffffe5252c68
Habibur Rahman <habibur0199mhr@gmail.com>
2024-09-27 11:21:43 +0600

    spring testing

---
+++ src/main/java/com/springboottestring/SpringBootProjectUnitTestApplication.java
@@ -0,0 +1,13 @@
+package com.springboottestring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProjectUnitTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProjectUnitTestApplication.class, args);
+	}
+
+}
