3171f20a3807ab2094f6fc8cf48ac74f508783ac
Habibur Rahman <habibur0199mhr@gmail.com>
2024-09-27 13:02:27 +0600

    h2 database custom config

---
+++ src/main/java/com/springboot/testing/SpringBootTestringApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.testing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTestringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTestringApplication.class, args);
+	}
+
+}
